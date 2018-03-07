78ef28faff96fe6fbf5241d8110c96e3c2c48d3f
gkeenley <gkeenley@uwo.ca>
2018-03-07 16:42:00 -0500

    Add files via upload

---
+++ floatingRectangles/js/glMatrixRef.js
@@ -0,0 +1,2 @@
+    // Refers to minified .js files needed for this program to run.
+    <script type="text/javascript" src="glMatrix-0.9.5.min.js"></script> // First minified .js file needed for program to run.
---
+++ floatingRectangles/js/webGLUtilsRef.js
@@ -0,0 +1,1 @@
+    <script type="text/javascript" src="webgl-utils.js"></script> // Second minified .js file needed for program to run. -->
